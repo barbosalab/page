f912b524eb53fe32aabff900b19558c7cdc6752c
Barbosa Lab <marcelojragro@gmail.com>
2025-11-11 14:32:19 -0500

    Change slideshow background images to JPG format
    Updated background images in slideshow settings.

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,9 +36,9 @@
 
 					// Images (in the format of 'url': 'alignment').
 						images: {
-							'images/bg01.png': 'center',
-							'images/bg02.png': 'center',
-							'images/bg03.png': 'center'
+							'images/bg01.jpg': 'center',
+							'images/bg02.jpg': 'center',
+							'images/bg03.jpg': 'center'
 						},
 
 					// Delay.
@@ -170,3 +170,4 @@
 
 
 })();
+
